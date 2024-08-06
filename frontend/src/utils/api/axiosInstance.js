5b4662594c79343b4224a3358b28ade002b9b190
nishanthan-k <krn.nishanthan@gmail.com>
2024-08-06 18:30:44 +0530

    feat: axios instance added

---
+++ frontend/src/utils/api/axiosInstance.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const protocol = import.meta.env.VITE_SERVER_PROTOCOL;
+const host = import.meta.env.VITE_SERVER_HOST;
+const port = import.meta.env.VITE_SERVER_PORT;
+
+const axiosInstance = axios.create({
+  baseURL: `${protocol}://${host}:${port}`,
+  headers: {
+    'Content-Type': 'application/json'
+  },
+})
+
+axiosInstance.interceptors.request.use((config) => {
+  config.data = config.data || {};
+  config.data.user_id = 1;
+  return config;
+}, (error) => {
+  console.log('API Error');
+  return Promise.reject(error)
+})
+
+export default axiosInstance;
