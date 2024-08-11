1a8ca58cf34632ebe3d3cb12838d821717011735
nishanthan-k <krn.nishanthan@gmail.com>
2024-08-11 12:35:21 +0530

    fix: vercel added for server

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import { verifyJWT } from './middlewares/auth.middleware.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
